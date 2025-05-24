e22dd47bbe090b3837f6f09eb6b77b564393b310
Thet Paing Soe <thetpaingsoe.ptn@gmail.com>
2025-05-24 18:59:30 +0700

    Configure for docker

--- fluxo-task-service/app/database.py
+++ fluxo-task-service/app/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+MYSQL_HOST = "db" #os.getenv("MYSQL_HOST", "localhost")
 MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
 MYSQL_DB = os.getenv("MYSQL_DB")
 
