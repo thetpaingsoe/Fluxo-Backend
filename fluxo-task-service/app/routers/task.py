ceb3434e0814f9f15fc80ad172dd57376d232dfa
Thet Paing Soe <thetpaingsoe.ptn@gmail.com>
2025-05-28 17:07:45 +0700

    Added Task Update End Point

--- fluxo-task-service/app/routers/task.py
+++ fluxo-task-service/app/routers/task.py
@@ -24,3 +24,9 @@ def read_task(task_id:int, db: Session = Depends(get_db)):
 def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
     return crud.create_task(db, task)
 
+@router.put("/{task_id}", response_model=schemas.TaskOut)
+def update_task(task_id: int, task: schemas.TaskUpdate, db : Session = Depends(get_db)):
+    updated_task = crud.update_task(db, task_id, task)
+    if updated_task is None : 
+        raise HTTPException(status_code=404, detail="Task Not Found")
+    return updated_task
