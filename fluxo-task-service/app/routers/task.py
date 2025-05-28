31d3aeb310d16e711115201f060b52d5d085d7b2
Thet Paing Soe <thetpaingsoe.ptn@gmail.com>
2025-05-28 17:03:08 +0700

    Added Get Task EndPoint

--- fluxo-task-service/app/routers/task.py
+++ fluxo-task-service/app/routers/task.py
@@ -16,6 +16,11 @@ def get_db():
 def read_tasks(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return crud.get_tasks(db, skip=skip, limit=limit)
 
+@router.get("/{task_id}", response_model=schemas.TaskOut)
+def read_task(task_id:int, db: Session = Depends(get_db)):
+    return crud.get_task(db, task_id)
+
 @router.post("/", response_model=schemas.TaskOut)
 def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
     return crud.create_task(db, task)
+
