320bdf2ca46460c65cef9f5db21b2ab98595176f
Thet Paing Soe <thetpaingsoe.ptn@gmail.com>
2025-05-30 16:14:50 +0700

    Init JWT Token

--- auth-service/app/auth.py
+++ auth-service/app/auth.py
@@ -1,11 +1,35 @@
 
 from fastapi import Request
+import jwt
 
 def login(request : Request):
     
     auth = request.headers.get("Authorization")
     
-    if not auth: 
-        return {"message": "Don't have Auth"}
-    else :
-        return{"message": "Welcome!"}
+
+def encode(request : Request):
+    # auth = request.headers.get("Authorization")
+    # if not auth:
+    #     return {"error": "Authorization header is missing"}
+    
+    try:
+        token = jwt.encode({"user": "example_user"}, "secret", algorithm="HS256")
+        return {"token": token}
+    except Exception as e:
+        return {"error": str(e)}
+    
+def decode(request : Request):
+    auth = request.headers.get("Authorization")
+    if not auth:
+        return {"error": "Authorization header is missing"}
+    
+    try:
+        token = auth.split(" ")[1]  # Assuming the format is "Bearer <token>"
+        decoded = jwt.decode(token, "secret", algorithms=["HS256"])
+        return {"decoded": decoded}
+    except jwt.ExpiredSignatureError:
+        return {"error": "Token has expired"}
+    except jwt.InvalidTokenError:
+        return {"error": "Invalid token"}
+    except Exception as e:
+        return {"error": str(e)}
--- auth-service/app/routers/route.py
+++ auth-service/app/routers/route.py
@@ -2,6 +2,14 @@ from fastapi import APIRouter, Request
 from .. import auth
 router = APIRouter(tags=["Auths"])
 
-@router.get("/login")
+@router.post("/login")
 def login(request: Request):    
     return auth.login(request)
+
+@router.get("/encode")
+def encode(request: Request):
+    return auth.encode(request)
+
+@router.get("/decode")
+def decode(request: Request):
+    return auth.decode(request)
